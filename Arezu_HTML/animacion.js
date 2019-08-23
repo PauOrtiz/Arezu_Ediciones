11200e934f3dfeecdd2a989ff5382dc7fd137caa
Paula Ortiz <sarkofy@hotmail.com>
2019-08-23 14:51:30 -0500

    h1,h2 efecto

---
+++ Arezu_HTML/animacion.js
@@ -0,0 +1,10 @@
+$("document").ready(function(){
+    $("header").mouseover(function(){
+     $("h1").animate({opacity:"0.5",
+     },500);  
+      });
+     $("section").mouseover(function(){
+      $("h2").animate({opacity:"0.8",
+       },2000);  
+     });  
+    }); 
