2bf9ba8863ae9cb9962db12e2b712c39dbcec4d4
Paula Ortiz <sarkofy@hotmail.com>
2019-08-29 15:15:42 -0500

    .mouseover,.animate,.css,.mouseout

--- Arezu_HTML/animacion.js
+++ Arezu_HTML/animacion.js
@@ -1,10 +1,16 @@
 $("document").ready(function(){
-    $("header").mouseover(function(){
-     $("h1").animate({opacity:"0.5",
-     },500);  
-      });
-     $("section").mouseover(function(){
-      $("h2").animate({opacity:"0.8",
-       },2000);  
-     });  
-    }); 
+  $("header").mouseover(function(){
+   $("h1").animate({opacity:"0.5",
+   },500);  
+    });
+    $("header").mouseout(function(){ 
+     $("h1").css("color", "orange");
+     $("h1").animate({opacity:"1",
+     },100);
+    });  
+      
+   $("section").mouseover(function(){
+    $("h2").animate({opacity:"0.8",
+     },2000);  
+   });  
+  }); 
